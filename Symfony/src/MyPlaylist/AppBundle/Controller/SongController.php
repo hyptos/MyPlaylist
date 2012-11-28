acfee6e151faed6d081b49fba1e21da077b7ab21
Dorian Garcia <freest@ymail.com>
2012-11-28 13:27:30 +0100

    Ajout edition et correction suppression

--- Symfony/src/MyPlaylist/AppBundle/Controller/SongController.php
+++ Symfony/src/MyPlaylist/AppBundle/Controller/SongController.php
@@ -59,7 +59,7 @@ class SongController extends Controller
                 $em->flush();
 
                 // On redirige vers la page de visualisation de la playlist nouvellement créé
-           return $this->redirect($this->generateUrl('MyPlaylist_viewSongId', array('id' => $playlist->getId())));
+                return $this->redirect($this->generateUrl('MyPlaylist_viewSongId', array('id' => $song->getId())));
             }
         }
 
@@ -68,10 +68,81 @@ class SongController extends Controller
         ));
     }
 
-    public function viewEditSongAction()
+     public function viewEditSongAction()
     {
-    	//Permet de modifier une chanson
-        return $this->render('MyPlaylistAppBundle:Song:edit.html.twig');
+        //Affiche toute les playlist disponible
+        // On récupère toute la table Playlist
+        $song = $this->getDoctrine()
+                         ->getManager()
+                         ->getRepository('MyPlaylistAppBundle:Song')
+                         ->findAll();
+
+        // $playlist est donc une instance de de l'entité Playlist
+
+        // Ou null si aucune playlist n'a été trouvé avec l'id $id
+        if($song === null)
+        {
+            throw $this->createNotFoundException('Song[id='.$id.'] inexistant.');
+        }
+        
+        return $this->render('MyPlaylistAppBundle:Song:viewEditSong.html.twig', array('song' => $song));
+    }
+    
+     public function editSongAction(Song $song)
+    {
+        //Permet de modifier une playlist
+               
+    {
+         $song = $this      ->getDoctrine()
+                            ->getManager()
+                            ->getRepository('MyPlaylistAppBundle:Song')
+                            ->find($song->getId()); 
+
+        // On crée le formulaire à partir de l'entité Playlist
+        $form   = $this->createFormBuilder($song)
+                ->add('title','text', array(
+                                        'attr' => array(
+                                        'placeholder' => $song->getTitle(),
+                                        'value' => '',
+                                            )))
+                ->add('length',       'text')
+                ->add('date_record',       'date')
+                ->add('filename',       'text') 
+                ->getForm();
+            
+
+        // On récupère la requête.
+        $request = $this->get('request');
+
+        // On vérifie qu'elle est de type « POST ».
+        if( $request->getMethod() == 'POST' )
+        {
+            // On fait le lien Requête <-> Formulaire.
+            $form->bind($request);
+
+            // On vérifie que les valeurs rentrées sont correctes.
+            // (Nous verrons la validation des objets en détail plus bas dans ce chapitre.)
+            if( $form->isValid() )
+            {
+                // On récupère les données de notre formulaire/
+                $name = $form["title"]->getData();
+                $song->setTitle($name);
+
+                //On modifie notre objet en BDD
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($song);
+                $em->flush();
+
+                // On redirige vers l'affichage de la playlist
+              return $this->redirect($this->generateUrl('MyPlaylist_viewSongId', array('id' => $song->getId())));
+            }
+        }
+
+        return $this->render('MyPlaylistAppBundle:Song:viewEdit.html.twig', array(
+            'form'      => $form->createView(),
+            'song'  => $song,
+        ));
+    }
     }
 
     //Permet de supprimer une chanson
@@ -82,7 +153,7 @@ class SongController extends Controller
 
         // On crée le formulaire à partir de l'entité Playlist
         $form = $this->createFormBuilder($song)
-            ->add('Title','entity', array(
+            ->add('title','entity', array(
                                                 'class'     => 'MyPlaylistAppBundle:Song',
                                                 'property'  => 'title',
                                                 'required'  => false))
